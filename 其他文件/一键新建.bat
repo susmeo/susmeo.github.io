fb348cf145d18606062c5c8b4c41df8e8678b217
susmeo <3094723062@qq.com>
2022-09-24 17:09:17 +0800

    Site updated: 2022-09-24 17:09:09

--- 其他文件/一键新建.bat
+++ 其他文件/一键新建.bat
@@ -1,4 +1,5 @@
 CHCP 65001
 CD C:\Users\Lenovo\Desktop\编程\我的网站
 set /p input=请输入要新建的文章名
-hexo new "%input%"
+copy C:\Users\Lenovo\Desktop\编程\我的网站\scaffolds\post.md C:\Users\Lenovo\Desktop\编程\我的网站\source\_drafts\%input%.md
+C:\Users\Lenovo\Desktop\编程\我的网站\source\_drafts\%input%.md
